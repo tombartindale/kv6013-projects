d67e79ea70f3a9d49a8712d73c99aa37f7f8529e
Tom Bartindale <tom@bartindale.com>
2024-08-30 12:15:52 +0100

    baseurl

--- projectlist/vue.config.js
+++ projectlist/vue.config.js
@@ -17,28 +17,7 @@ module.exports = {
       // new ImageminWebpWebpackPlugin(),
     ],
   },
-  // chainWebpack: (config) => {
-  //   config
-  //     .plugin("obsolete")
-  //     .use(
-  //       new ObsoleteWebpackPlugin({
-  //         // browsers: ["ie 11"],
-  //         name: "obsolete",
-  //         position: "beforeend",
-  //         template:
-  //           '<div class="fixed-full bg-white fullscreen z-max"><div class="absolute-center text-center"><h4>Sorry, Your browser is not supported</h4></div></div>',
-  //         promptOnNonTargetBrowser: true,
-  //       })
-  //     )
-  //     .end()
-  //     .plugin("scriptext")
-  //     .use(
-  //       new ScriptExtHtmlWebpackPlugin({
-  //         async: "obsolete",
-  //       })
-  //     )
-  //     .end();
-  // },
+  publicPath: process.env.NODE_ENV === "production" ? "/kv6013-projects/" : "/",
   pluginOptions: {
     quasar: {
       // importStrategy: "kebab",
