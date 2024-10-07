ae6c25e925e20fd3b08611bf3f96eec90d14e51e
Tom Bartindale <tom@bartindale.com>
2024-10-07 10:59:14 +0100

    preprocess

---
+++ projectlist/preprocess.js
@@ -0,0 +1,14 @@
+const data = require("./data/projects.json");
+const fs = require("fs");
+const _ = require("lodash");
+
+let newdata = [];
+
+//strip out fields from data:
+
+for (let d of data) {
+  newdata.push(_.omit(d, ["TargetedStudents", "TargetedStudents#Claims"]));
+}
+
+//save back data:
+fs.writeFileSync("./data/projects.json", JSON.stringify(newdata));
