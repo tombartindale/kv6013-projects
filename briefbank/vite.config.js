44af841f939384fa6541c89941268e753307350c
Tom Bartindale <tom@bartindale.com>
2026-06-16 13:10:15 +0100

    base url

--- briefbank/vite.config.js
+++ briefbank/vite.config.js
@@ -4,6 +4,7 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/kv6013-projects/' : '/',
   plugins: [
     vue({ template: { transformAssetUrls } }),
     quasar({
